8e7d33ae074c2d9a8868606c920333fa91e769ba
Stephanie Ashe <estefannyyy@Edwards-Mac-mini.local>
2019-04-30 17:50:19 -0400

    update roll button and add code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let dicearray = [ "dice1", "dice2", "dice3", "dice4", "dice5", "dice6" ]
 
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageview2: UIImageView!
+    
     override func viewDidLoad() {
+        changedicefaces()
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func Rollbuttonpressed(_ sender: Any) {
+       changedicefaces()
+    
+        
+    }
+    
+    func changedicefaces () {
+        let random1 = Int.random( in: 0 ... 5)
+        let random2 = Int.random( in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: dicearray[random1])
+        diceImageview2.image = UIImage(named: dicearray[random2])}
+    
 
+        
+    
+    
 }
+
+
 
